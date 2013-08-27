481a2331e41e7b99127b87e7593854d861a5090b
dbuttig <ddbstlmo@hotmail.com>
2013-08-27 04:25:05 +0000

    base project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+D6CharacterCreator::Application.config.secret_key_base = '4b37d1f03aafcdea03079a574c947e14423f74bb7245cc1ac39deb2346e2e327886b6f6470e28c8be76ddce9b69d1b7591e1c37ca2375b4fa790ae84f8a66b97'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+D6CharacterCreator::Application.config.session_store :cookie_store, key: '_D6-Character-Creator_session'
